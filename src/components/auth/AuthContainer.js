3501ae84b50b12f3c02aaed6ca0e4ea1b313bf77
Christopher-Deo <chris@christopherdeo.com>
2023-02-02 16:03:15 -0600

    completed base code for Reset component

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Login from '../../components/auth/Login';
 import Register from "../../components/auth/Register"
+import Reset from "../../components/auth/Reset"
 import "./AuthContainer.scss"
+
+
 const AuthContainer = () => {
   return (
     <section className='--flex-center --100vh'>
       <div className="container box">
         {/* <Login /> */}
-        <Register />
+        {/* <Register /> */}
+        <Reset />
       </div>
       
     </section>
--- src/components/auth/Reset.js
+++ src/components/auth/Reset.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import resetImg from "../../assets/forgot.svg";
+import { AiOutlineClose } from 'react-icons/ai';
+
 
 const Reset = () => {
     return (
-        <div>
-            <h1>Reset Password</h1>
+        <div className='main-container --flex-center'>
+            
+            <div className='form-container reset --bg-grey'>
+                <form className='--form-control'>
+                    <h2 className='--color-danger --text-center'>Reset Password</h2>
+                    <input type='email' className="--width-100"
+                        placeholder="Enter your email" />
+
+                    <button className='--btn --btn-primary --btn-block'>Reset Password</button>
+
+                    <span className="--text-sm --block --text-center">Check your email for reset link!
+
+                    </span>
+                    <div className="close">
+                       <AiOutlineClose />
+                    </div>
+                </form>
+            </div>
+            <div className='img-container'>
+                <img src={resetImg} alt='register screen' />
+            </div>
         </div>
     );
 };
