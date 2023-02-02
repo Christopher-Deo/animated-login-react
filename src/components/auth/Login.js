e9423ba739a30714e5ecf32d27e819eb16242225
Christopher-Deo <chris@christopherdeo.com>
2023-02-02 13:02:44 -0600

    Setup initial file structure

---
+++ src/components/auth/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      // check Lower and Uppercase
+          match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)
+
+          // Check For Numbers
+          match(/([0-9])/)
+
+          // Check For Special char
+          match(/([!,%,&,@,#,$,^,*,?,_,~])/)
+    </div>
+  )
+}
+
+export default Login
