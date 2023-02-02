aa1babf48afd909138c6262b869b5f63724f1f5e
Christopher-Deo <chris@christopherdeo.com>
2023-02-02 16:37:46 -0600

    completed logic code for all components

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,21 +1,65 @@
-import React from 'react'
-import Login from '../../components/auth/Login';
-import Register from "../../components/auth/Register"
-import Reset from "../../components/auth/Reset"
-import "./AuthContainer.scss"
-
+import { useState } from "react";
+import "./AuthContainer.scss";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
 
 const AuthContainer = () => {
-  return (
-    <section className='--flex-center --100vh'>
-      <div className="container box">
-        {/* <Login /> */}
-        {/* <Register /> */}
-        <Reset />
-      </div>
-      
-    </section>
-  )
-}
+  const [auth, setAuth] = useState({
+    login: true,
+    register: false,
+    reset: false,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+  // const [login, setLogin] = useState(true);
+  // const [register, setRegister] = useState(false);
+  // const [reset, setReset] = useState(false);
 
-export default AuthContainer
+  const handleLogin = () => {
+    setAuth({ login: true, register: false, reset: false });
+    // setReset(false);
+    // setLogin(true);
+    // setRegister(false);
+  };
+
+  const handleRegister = () => {
+    setAuth({ login: false, register: true, reset: false });
+    // setLogin(false);
+    // setRegister(true);
+    // setReset(false);
+  };
+  const handleReset = () => {
+    setAuth({ login: false, register: false, reset: true });
+    // setLogin(false);
+    // setReset(true);
+    // setRegister(false);
+  };
+
+  return (
+    <section className="--flex-center --100vh">
+      <div className="container box">
+        {auth.login && (
+          <Login
+            onRegister={handleRegister}
+            onReset={handleReset}
+            onShowPassword={showPassword}
+            onTogglePassword={handleTogglePassword}
+          />
+        )}
+        {auth.register && (
+          <Register
+            onLogin={handleLogin}
+            onShowPassword={showPassword}
+            onTogglePassword={handleTogglePassword}
+          />
+        )}
+        {auth.reset && <Reset onLogin={handleLogin} />}
+      </div>
+    </section>
+  );
+};
+
+export default AuthContainer;
--- src/components/auth/Reset.js
+++ src/components/auth/Reset.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 import resetImg from "../../assets/forgot.svg";
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose } from "react-icons/ai";
 
-
-const Reset = () => {
+const Reset = ({ onLogin }) => {
     return (
-        <div className='main-container --flex-center'>
-            
-            <div className='form-container reset --bg-grey'>
-                <form className='--form-control'>
-                    <h2 className='--color-danger --text-center'>Reset Password</h2>
-                    <input type='email' className="--width-100"
-                        placeholder="Enter your email" />
+        <div className="main-container --flex-center">
+            <div className="form-container reset">
+                <form className="--form-control">
+                    <h2 className="--color-danger --text-center">Reset</h2>
 
-                    <button className='--btn --btn-primary --btn-block'>Reset Password</button>
+                    <input type="email" className="--width-100" placeholder="Email" />
 
-                    <span className="--text-sm --block --text-center">Check your email for reset link!
+                    <button className="--btn --btn-primary --btn-block">
+                        Reset Password
+                    </button>
 
+                    <span className="--text-sm --block --text-center">
+                        We will send you a reset link!!!
                     </span>
-                    <div className="close">
-                       <AiOutlineClose />
+                    <div className="close" onClick={onLogin}>
+                        <AiOutlineClose color="red" />
                     </div>
                 </form>
             </div>
-            <div className='img-container'>
-                <img src={resetImg} alt='register screen' />
+            <div className="img-container">
+                <img src={resetImg} alt="login" />
             </div>
         </div>
     );
